78c594fec15ba96a3a054fe31b5a97032dcac245
edp6301 <>
2020-07-20 10:43:51 -0600

    remove column

--- db/migrate/20200719201555_removecolumn_topedido.rb
+++
@@ -1,6 +0,0 @@
-class RemovecolumnTopedido < ActiveRecord::Migration[6.0]
-  def change
-    remove_reference :pedidos, :tipo_entrega, index: true, foreign_key: true
-    remove_reference :pedidos, :responsable, index: true, foreign_key: true
-  end
-end
