a9a23d84fd9c0ef32055a5b7cc809ebfe984dcb0
edp6301 <>
2020-07-30 11:19:11 -0600

    using notific and views deploy 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :authenticate_user!
     protected
   
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, :current_user, keys: [:first_name, :last_name, :club_id, :rol_id, :email, :current_user])
-      helper_method :current_user
+      devise_parameter_sanitizer.permit(:sign_up, :current_user, keys: [:first_name, :last_name, :club_id, :rol_id, :email])
+      
     end
     
 end
